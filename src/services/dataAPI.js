a2953a8dedfcdb0f262a9de8ed5bd68e020309fa
Bhuvnesh Chadha <bhuvneshchadha6@gmail.com>
2026-08-30 00:44:21 +0530

    build work

--- src/services/dataAPI.js
+++ src/services/dataAPI.js
@@ -2,7 +2,7 @@
 export async function homePageData(language) {
   try {
     const response = await fetch(
-      `${"https://jiosaavn-api-sigma-sandy.vercel.app"}/api/modules?language=${language.toString()}`,
+      `${"https://saavn.zendax.me/api"}/api/modules?language=${language.toString()}`,
       {
         next: {
           revalidate: 14400,
